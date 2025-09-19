1528c62d57be87e31b5efde43a69cffe28ecea24
Gowtham1809 <gowthamrajmamundi2002@email.com>
2025-09-19 20:40:06 +0530

    changed env

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000";
+const BACKEND_URL =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 const API_URL = `${BACKEND_URL}/api/doctors`;
 
 export const getDoctors = () => axios.get(API_URL);
